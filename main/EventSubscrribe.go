c803db7c75e24f6f6dd3ae2e60598bbcba410a8b
Iziop <abkozhakhmetov@gmail.com>
2022-08-25 14:17:58 +0300

    update read last block number

--- main/EventSubscrribe.go
+++ main/EventSubscrribe.go
@@ -6,7 +6,6 @@ import (
 	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"log"
@@ -28,79 +27,81 @@ func main() {
 	//	Addresses: []common.Address{contractAddress},
 	//}
 
-	headers := make(chan *types.Header)
-	sub, err := client.SubscribeNewHead(context.Background(), headers)
-	if err != nil {
-		log.Fatal(err)
-	}
+	//headers := make(chan *types.Header)
+	//sub, err := client.SubscribeNewHead(context.Background(), headers)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
 	var lastReadBlock = int64(11257)
 
 	for {
-		select {
-		case err := <-sub.Err():
+
+		//block, err := client.BlockByHash(context.Background(), header.Hash())
+		//if err != nil {
+		//	log.Fatal(err)
+		//}
+
+		headerByNumber, err := client.HeaderByNumber(context.Background(), nil)
+		if err != nil {
 			log.Fatal(err)
-		case header := <-headers:
-			block, err := client.BlockByHash(context.Background(), header.Hash())
+		}
+
+		fmt.Println(lastReadBlock)
+		fmt.Println(headerByNumber.Number)
+
+		queryFilter := ethereum.FilterQuery{
+			FromBlock: big.NewInt(lastReadBlock + 1),
+			ToBlock:   headerByNumber.Number,
+			Addresses: []common.Address{contractAddress},
+		}
+
+		logs, err := client.FilterLogs(context.Background(), queryFilter)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		contractAbi, err := abi.JSON(strings.NewReader(contractAbi))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		type LogTransfer struct {
+			From   common.Address
+			To     common.Address
+			Tokens *big.Int
+		}
+		logTransferSig := []byte("Transfer(address,address,uint256)")
+		logTransferSigHash := crypto.Keccak256Hash(logTransferSig)
+
+		for _, vLog := range logs {
+
+			err, _ := contractAbi.Unpack("Transfer", vLog.Data)
 			if err != nil {
 				log.Fatal(err)
 			}
-			fmt.Println(lastReadBlock)
-			fmt.Println(block.Number().Int64()) // 3477413
 
-			queryFilter := ethereum.FilterQuery{
-				FromBlock: big.NewInt(lastReadBlock),
-				ToBlock:   big.NewInt(block.Number().Int64()),
-				Addresses: []common.Address{contractAddress},
-			}
+			switch vLog.Topics[0].Hex() {
+			case logTransferSigHash.Hex():
+				fmt.Printf("Log Name: Transfer\n")
 
-			logs, err := client.FilterLogs(context.Background(), queryFilter)
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			contractAbi, err := abi.JSON(strings.NewReader(contractAbi))
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			type LogTransfer struct {
-				From   common.Address
-				To     common.Address
-				Tokens *big.Int
-			}
-			logTransferSig := []byte("Transfer(address,address,uint256)")
-			logTransferSigHash := crypto.Keccak256Hash(logTransferSig)
-
-			for _, vLog := range logs {
+				var transferEvent LogTransfer
 
 				err, _ := contractAbi.Unpack("Transfer", vLog.Data)
 				if err != nil {
 					log.Fatal(err)
 				}
 
-				switch vLog.Topics[0].Hex() {
-				case logTransferSigHash.Hex():
-					fmt.Printf("Log Name: Transfer\n")
-
-					var transferEvent LogTransfer
-
-					err, _ := contractAbi.Unpack("Transfer", vLog.Data)
-					if err != nil {
-						log.Fatal(err)
-					}
-
-					transferEvent.From = common.HexToAddress(vLog.Topics[1].Hex())
-					transferEvent.To = common.HexToAddress(vLog.Topics[2].Hex())
-					fmt.Printf("From: %s\n", transferEvent.From.Hex())
-					fmt.Printf("To: %s\n", transferEvent.To.Hex())
-					fmt.Printf("Hex TokenId: %s\n", vLog.Topics[3].Big())
-				}
+				transferEvent.From = common.HexToAddress(vLog.Topics[1].Hex())
+				transferEvent.To = common.HexToAddress(vLog.Topics[2].Hex())
+				fmt.Printf("From: %s\n", transferEvent.From.Hex())
+				fmt.Printf("To: %s\n", transferEvent.To.Hex())
+				fmt.Printf("Hex TokenId: %s\n", vLog.Topics[3].Big())
 			}
-			lastReadBlock = block.Number().Int64()
-
 		}
+		//lastReadBlock = block.Number().Int64()
+		lastReadBlock = headerByNumber.Number.Int64()
 
-		time.Sleep(1 * time.Minute)
+		time.Sleep(4 * time.Minute)
 	}
 
 }
