0646aae26a3daf95b4c3efcba83f0e0c02ec9ba4
Iziop <abkozhakhmetov@gmail.com>
2022-09-12 16:41:51 +0300

    added com

--- main/EventSubscrribe.go
+++ main/EventSubscrribe.go
@@ -16,6 +16,7 @@ import (
 
 var contractAbi = `[{"inputs":[{"internalType":"string","name":"initMessage","type":"string"}],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"string","name":"oldStr","type":"string"},{"indexed":false,"internalType":"string","name":"newStr","type":"string"}],"name":"UpdatedMessages","type":"event"},{"inputs":[],"name":"message","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"string","name":"newMessage","type":"string"}],"name":"update","outputs":[],"stateMutability":"nonpayable","type":"function"}]`
 
+//believe that amount of owner' tokens will not be too much
 func findAndDelete(s []*big.Int, item *big.Int) []*big.Int {
 	index := 0
 	for _, i := range s {
